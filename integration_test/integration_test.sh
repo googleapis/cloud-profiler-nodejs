e6ac040b2174fd7774262e4fbebb3b33575734c6
Maggie Nolan <nolanmar@google.com>
2018-02-16 07:36:08 -0800

    add integration.sh

---
+++ integration_test/integration_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Fail on any error.
+set -eo pipefail
+
+# Display commands being run.
+set -x
+
+SERVICE_KEY="${KOKORO_KEYSTORE_DIR}/72935_cloud-profiler-e2e-service-account-key"
+COMMIT=$(git rev-parse HEAD)
+
+export GCLOUD_TESTS_NODEJS_PROJECT_ID="cloud-profiler-e2e"
+export GCLOUD_TESTS_NODEJS_ZONE="us-west1-a"
+export GOOGLE_APPLICATION_CREDENTIALS="${SERVICE_KEY}"
+
+# Move test to go path.
+export GOPATH="$HOME/go"
+mkdir -p "$GOPATH/src"
+cp -R "git/cloud-profiler-nodejs/integration_test" "$GOPATH/src"
+
+# Run test.
+cd "$GOPATH/src/integration_test"
+go get -d -t ./
+go test -timeout=30m -parallel=5 -tags=integration -run TestAgentIntegration -commit="$COMMIT"
