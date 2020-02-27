d8f26194f05885092e9259665529922da57e5d04
summerji <summerji@google.com>
2020-02-27 15:19:17 -0800

    chore: update jsdoc by adding protos and remove double qoutes

--- .jsdoc.js
+++ .jsdoc.js
@@ -31,7 +31,8 @@ module.exports = {
   source: {
     excludePattern: '(^|\\/|\\\\)[._]',
     include: [
-      'out/src'
+      'out/src',
+      'protos'
     ],
     includePattern: '\\.js$'
   },
@@ -42,7 +43,7 @@ module.exports = {
     systemName: '@google-cloud/profiler',
     theme: 'lumen',
     default: {
-      "outputSourceFiles": false
+      outputSourceFiles: false
     }
   },
   markdown: {
