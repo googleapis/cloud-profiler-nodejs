8f35814bbb56c8f96fcebbbb69fc2300e93fee3c
Aaron Abbott <aaronabbott@google.com>
2024-07-09 18:28:51 +0000

    use main instead of HEAD

--- system-test/integration_test.sh
+++ system-test/integration_test.sh
@@ -41,7 +41,7 @@ go version
 # dependencies specified by their go.mod files. This reduces the likelihood of
 # dependencies breaking this test.
 go mod init e2e
-retry go get cloud.google.com/go/profiler@HEAD
+retry go get cloud.google.com/go/profiler@main
 retry go mod tidy
 retry go test -c -tags=integration .
 
